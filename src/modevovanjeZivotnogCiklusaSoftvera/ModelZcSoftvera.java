48248a8a1ce9db4662893e368a0ed484d1b1adfc
d.popovic89@gmail.com <d.popovic@gmail.com>
2016-01-25 20:47:25 +0100

    Izbacivanje komentara iz modela

--- src/modevovanjeZivotnogCiklusaSoftvera/ModelZcSoftvera.java
+++ src/modevovanjeZivotnogCiklusaSoftvera/ModelZcSoftvera.java
@@ -1,75 +1,18 @@
-/***********************************************************************
- * Module:  ModelZcSoftvera.java
- * Author:  Pop
- * Purpose: Defines the Class ModelZcSoftvera
- ***********************************************************************/
 
 package modevovanjeZivotnogCiklusaSoftvera;
 
 import java.util.*;
 
-/** @pdOid c4d4a276-6bbd-4e82-8330-d298a00fc248 */
 public class ModelZcSoftvera {
-   /** @pdOid 2c2fa650-1d69-49bf-876f-d1545bd6f036 */
+  
    public int oznaka;
-   /** @pdOid cce71fff-36ef-4a23-947c-8bab9adeeb7e */
-   public java.lang.String naziv;
-   /** @pdOid 9cb06a3f-866a-4007-aea5-8dd95a717ac8 */
-   public java.lang.String skraceniNaziv;
-   /** @pdOid 45f060de-b59d-4cfc-af49-bd6585bf6056 */
-   public java.lang.String opis;
    
-   /** @pdRoleInfo migr=no name=StrukturaModela assc=struktura coll=java.util.Collection impl=java.util.HashSet mult=0..* type=Composition */
-   public java.util.Collection<StrukturaModela> strukturaModela;
+   public String naziv;
    
+   public String skraceniNaziv;
    
-   /** @pdGenerated default getter */
-   public java.util.Collection<StrukturaModela> getStrukturaModela() {
-      if (strukturaModela == null)
-         strukturaModela = new java.util.HashSet<StrukturaModela>();
-      return strukturaModela;
-   }
+   public String opis;
+      
+   public List<StrukturaModela> strukturaModela;
    
-   /** @pdGenerated default iterator getter */
-   public java.util.Iterator getIteratorStrukturaModela() {
-      if (strukturaModela == null)
-         strukturaModela = new java.util.HashSet<StrukturaModela>();
-      return strukturaModela.iterator();
-   }
-   
-   /** @pdGenerated default setter
-     * @param newStrukturaModela */
-   public void setStrukturaModela(java.util.Collection<StrukturaModela> newStrukturaModela) {
-      removeAllStrukturaModela();
-      for (java.util.Iterator iter = newStrukturaModela.iterator(); iter.hasNext();)
-         addStrukturaModela((StrukturaModela)iter.next());
-   }
-   
-   /** @pdGenerated default add
-     * @param newStrukturaModela */
-   public void addStrukturaModela(StrukturaModela newStrukturaModela) {
-      if (newStrukturaModela == null)
-         return;
-      if (this.strukturaModela == null)
-         this.strukturaModela = new java.util.HashSet<StrukturaModela>();
-      if (!this.strukturaModela.contains(newStrukturaModela))
-         this.strukturaModela.add(newStrukturaModela);
-   }
-   
-   /** @pdGenerated default remove
-     * @param oldStrukturaModela */
-   public void removeStrukturaModela(StrukturaModela oldStrukturaModela) {
-      if (oldStrukturaModela == null)
-         return;
-      if (this.strukturaModela != null)
-         if (this.strukturaModela.contains(oldStrukturaModela))
-            this.strukturaModela.remove(oldStrukturaModela);
-   }
-   
-   /** @pdGenerated default removeAll */
-   public void removeAllStrukturaModela() {
-      if (strukturaModela != null)
-         strukturaModela.clear();
-   }
-
 }
--- src/modevovanjeZivotnogCiklusaSoftvera/StrukturaModela.java
+++ src/modevovanjeZivotnogCiklusaSoftvera/StrukturaModela.java
@@ -1,149 +1,15 @@
-/***********************************************************************
- * Module:  StrukturaModela.java
- * Author:  Pop
- * Purpose: Defines the Class StrukturaModela
- ***********************************************************************/
-
 package modevovanjeZivotnogCiklusaSoftvera;
 
 import java.util.*;
 
-/** @pdOid 17372300-5d5e-4495-965c-3ad88757907d */
 public class StrukturaModela {
-   /** @pdOid 362203f1-4a7d-4eed-bea7-f5bc74da0954 */
+
    public double nivo;
-   /** @pdOid 2b63550e-818c-4574-bc4b-1cab537db5ee */
+
    public double polozajUNivou;
+      
+   public List<StrukturaModela> strukturaModelaB;
    
-   /** @pdRoleInfo migr=no name=StrukturaModela assc=hiperlinkovi coll=java.util.Collection impl=java.util.HashSet mult=0..* */
-   public java.util.Collection<StrukturaModela> strukturaModelaB;
-   /** @pdRoleInfo migr=no name=StrukturaModela assc=hiperlinkovi coll=java.util.Collection impl=java.util.HashSet mult=0..* side=A */
-   public java.util.Collection<StrukturaModela> strukturaModelaA;
-   
-   
-   /** @pdGenerated default getter */
-   public java.util.Collection<StrukturaModela> getStrukturaModelaB() {
-      if (strukturaModelaB == null)
-         strukturaModelaB = new java.util.HashSet<StrukturaModela>();
-      return strukturaModelaB;
-   }
-   
-   /** @pdGenerated default iterator getter */
-   public java.util.Iterator getIteratorStrukturaModelaB() {
-      if (strukturaModelaB == null)
-         strukturaModelaB = new java.util.HashSet<StrukturaModela>();
-      return strukturaModelaB.iterator();
-   }
-   
-   /** @pdGenerated default setter
-     * @param newStrukturaModelaB */
-   public void setStrukturaModelaB(java.util.Collection<StrukturaModela> newStrukturaModelaB) {
-      removeAllStrukturaModelaB();
-      for (java.util.Iterator iter = newStrukturaModelaB.iterator(); iter.hasNext();)
-         addStrukturaModelaB((StrukturaModela)iter.next());
-   }
-   
-   /** @pdGenerated default add
-     * @param newStrukturaModela */
-   public void addStrukturaModelaB(StrukturaModela newStrukturaModela) {
-      if (newStrukturaModela == null)
-         return;
-      if (this.strukturaModelaB == null)
-         this.strukturaModelaB = new java.util.HashSet<StrukturaModela>();
-      if (!this.strukturaModelaB.contains(newStrukturaModela))
-      {
-         this.strukturaModelaB.add(newStrukturaModela);
-         newStrukturaModela.addStrukturaModelaA(this);      
-      }
-   }
-   
-   /** @pdGenerated default remove
-     * @param oldStrukturaModela */
-   public void removeStrukturaModelaB(StrukturaModela oldStrukturaModela) {
-      if (oldStrukturaModela == null)
-         return;
-      if (this.strukturaModelaB != null)
-         if (this.strukturaModelaB.contains(oldStrukturaModela))
-         {
-            this.strukturaModelaB.remove(oldStrukturaModela);
-            oldStrukturaModela.removeStrukturaModelaA(this);
-         }
-   }
-   
-   /** @pdGenerated default removeAll */
-   public void removeAllStrukturaModelaB() {
-      if (strukturaModelaB != null)
-      {
-         StrukturaModela oldStrukturaModela;
-         for (java.util.Iterator iter = getIteratorStrukturaModelaB(); iter.hasNext();)
-         {
-            oldStrukturaModela = (StrukturaModela)iter.next();
-            iter.remove();
-            oldStrukturaModela.removeStrukturaModelaA(this);
-         }
-      }
-   }
-   /** @pdGenerated default getter */
-   public java.util.Collection<StrukturaModela> getStrukturaModelaA() {
-      if (strukturaModelaA == null)
-         strukturaModelaA = new java.util.HashSet<StrukturaModela>();
-      return strukturaModelaA;
-   }
-   
-   /** @pdGenerated default iterator getter */
-   public java.util.Iterator getIteratorStrukturaModelaA() {
-      if (strukturaModelaA == null)
-         strukturaModelaA = new java.util.HashSet<StrukturaModela>();
-      return strukturaModelaA.iterator();
-   }
-   
-   /** @pdGenerated default setter
-     * @param newStrukturaModelaA */
-   public void setStrukturaModelaA(java.util.Collection<StrukturaModela> newStrukturaModelaA) {
-      removeAllStrukturaModelaA();
-      for (java.util.Iterator iter = newStrukturaModelaA.iterator(); iter.hasNext();)
-         addStrukturaModelaA((StrukturaModela)iter.next());
-   }
-   
-   /** @pdGenerated default add
-     * @param newStrukturaModela */
-   public void addStrukturaModelaA(StrukturaModela newStrukturaModela) {
-      if (newStrukturaModela == null)
-         return;
-      if (this.strukturaModelaA == null)
-         this.strukturaModelaA = new java.util.HashSet<StrukturaModela>();
-      if (!this.strukturaModelaA.contains(newStrukturaModela))
-      {
-         this.strukturaModelaA.add(newStrukturaModela);
-         newStrukturaModela.addStrukturaModelaB(this);      
-      }
-   }
-   
-   /** @pdGenerated default remove
-     * @param oldStrukturaModela */
-   public void removeStrukturaModelaA(StrukturaModela oldStrukturaModela) {
-      if (oldStrukturaModela == null)
-         return;
-      if (this.strukturaModelaA != null)
-         if (this.strukturaModelaA.contains(oldStrukturaModela))
-         {
-            this.strukturaModelaA.remove(oldStrukturaModela);
-            oldStrukturaModela.removeStrukturaModelaB(this);
-         }
-   }
-   
-   /** @pdGenerated default removeAll */
-   public void removeAllStrukturaModelaA() {
-      if (strukturaModelaA != null)
-      {
-         StrukturaModela oldStrukturaModela;
-         for (java.util.Iterator iter = getIteratorStrukturaModelaA(); iter.hasNext();)
-         {
-            oldStrukturaModela = (StrukturaModela)iter.next();
-            iter.remove();
-            oldStrukturaModela.removeStrukturaModelaB(this);
-         }
-      }
-   }
+   public List<StrukturaModela> strukturaModelaA;
 
 }
